8535fc3e89dd44a6942c4307ca530c2b5738bf12
swetha <root@ip-172-31-42-48.eu-north-1.compute.internal>
2026-01-31 08:54:20 +0000

    mc3

--- myapps.java
+++ myapps.java
@@ -2,5 +2,7 @@ public class demo {
 public static void main(String[] args) {
 System.out.println("master-branch");
 System.out.println("GM");
+System.out.println("prod branch");
+System.out.println("Ge");
 }
 }
